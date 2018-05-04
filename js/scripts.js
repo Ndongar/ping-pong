88cb6db37990dfa0b83e81e4ea3eb58a4cad1823
Beatrice Wangui <ndongabeatrice97@gmail.com>
2018-05-04 17:24:11 +0300

    script.js added

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,32 @@
-$(document).ready(function) {
-  $("form#ping").submit(function(event) {
-    event.preventDefault();
-    var ping pong = parseInt($("input#number").val());
-    var result = ping pong(number);
-    $("#result").text(result);
-  });
+//Business Logic
+/* var input = parseInt(prompt('Enter a number')); */
+
+var pingPong = function(input) {
+ var result = [];
+ for(var i = 1; i <= input; i++){
+    if (i % 3 === 0 && i % 5 === 0){
+     result.push('ping-pong');
+   }else    if(i % 3 === 0){
+     result.push('ping');
+   } else if (i % 5 === 0){
+     result.push('pong');
+   } else {
+     result.push(i);
+ }
+ }
+return(result)
+};
+
+//UI logic
+$(document).ready(function(){
+    $('#ping').submit(function(e){
+   $("#result").empty();
+     e.preventDefault();
+   var input = parseInt($('input#number').val());
+     var output = pingPong(input);
+
+   output.forEach(function(value) {
+   $("#result").append('<li>' + value + '</li>');
+ });
+});
 });
